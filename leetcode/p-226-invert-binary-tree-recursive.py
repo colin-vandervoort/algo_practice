0a155d018ae7f4dd252c906b333bb55ce88b83ba
Colin VanDervoort <colin.vandervoort.42@gmail.com>
2022-05-29 18:54:25 -0700

    recursive inversion of binary tree

---
+++ leetcode/p-226-invert-binary-tree-recursive.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return None
+        temp = root.right
+        root.right = self.invertTree(root.left) if root.left else None
+        root.left = self.invertTree(temp) if temp else None
+        return root
