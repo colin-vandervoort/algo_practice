26e3c58c3b76270fdc7627ab28654c2c286b3097
Colin <colin.vandervoort.42@gmail.com>
2025-08-26 11:55:04 -0700

    chore: exclude cookiecutter dir from pytest discovery instead of scoping to solutions dir

--- python/tools/lc_new_py.py
+++ python/tools/lc_new_py.py
@@ -2,16 +2,15 @@ if __name__ == "__main__":
     from cookiecutter.main import cookiecutter
     import os
 
-    base_project_directory = os.path.join(os.path.dirname(__file__), "..", "..")
     base_project_directory = os.path.abspath(
         os.path.join(os.path.dirname(__file__), "..", "..")
     )
     py_lc_solutions_dir = os.path.abspath(
-        os.path.join(base_project_directory, "py", "solutions", "leetcode")
+        os.path.join(base_project_directory, "python", "solutions", "leetcode")
     )
     cookiecutter_template_python_solution = os.path.abspath(
         os.path.join(
-            base_project_directory, "py", "tools", "cookiecutter-leetcode-problem"
+            base_project_directory, "python", "tools", "cookiecutter-leetcode-problem"
         )
     )
 
